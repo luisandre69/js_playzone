97b09b0338cdb862db9bd1203b8d4d80b6cd6790
luisandre69 <luisandre_69@hotmail.com>
2018-09-28 23:13:59 +0100

    update forloop.js comments
    create whileloop increment and decrement test

--- whileloop.js
+++ whileloop.js
@@ -1,21 +1,25 @@
-//For Loop with increment value
+//While loop with increment value
 
 //Exercise 1 - Print a table of any nubmber
 var readline = require('readline-sync');
 
 var number = readline.question("Select the number to create the table of the number?: " + '\n');
 
-for(i=1;i<=10;i++)
+var i = 1;
+while(i<=10)
 {
     console.log(number + "*" + i + "=" + number*i)
+    i++;
 }
 
-//For loop with decrement value
+//While loop with decrement value
 //Exercise 2 - Print a table of any number in the reverse order
 
 var revnumber = readline.question("Select the number to create a reverse table of numbers?" + '\n');
 
-for (i=10;i>=1;i--)
+var i = 10;
+while(i>=1)
 {
     console.log(revnumber + "*" + i + "=" + revnumber*i)
+    i--
 }
